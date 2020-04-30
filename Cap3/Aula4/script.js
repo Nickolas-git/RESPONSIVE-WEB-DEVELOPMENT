7e73059a4db4d12d45d01e41ebab843ae5b3abd2
Nickolas-git <60943280+Nickolas-git@users.noreply.github.com>
2020-04-30 10:44:18 -0300

    Adição de aula

---
+++ Cap3/Aula4/script.js
@@ -0,0 +1,28 @@
+window.onload = function(){
+    console.log("Adição 4+4: ")
+    console.log(4+4)
+
+    console.log("Subtração 4-2: ")
+    console.log(4-2)
+
+    console.log("Multiplicação 4x2: ")
+    console.log(4*2)
+
+    console.log("Divisão 4/2: ")
+    console.log(4/2)
+
+    console.log("Resto de divisão de 10 por 3: ")
+    console.log(10%3)
+
+    var num=5
+    console.log("Valor de num: "+num)
+
+    num = Math.pow(5,2)
+    console.log("Valor de num: "+num)
+
+    num = num - this.Math.random()
+    console.log("Valor de num: "+num)
+
+    num = num * this.Math.PI
+    console.log("valor de num: "+num)
+}
