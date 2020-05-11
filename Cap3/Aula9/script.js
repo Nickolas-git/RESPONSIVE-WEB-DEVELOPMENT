de0b7e16400a7ee1bce3dce1607b2a7e5d772720
Nickolas-git <60943280+Nickolas-git@users.noreply.github.com>
2020-05-11 07:35:49 -0300

    adição de aula no capítulo de javascript

---
+++ Cap3/Aula9/script.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+   var oBotao = document.getElementById("botao")
+   var oDiv = document.getElementById("cidades")
+
+   oBotao.onclick = function(){
+    var xhttp = new XMLHttpRequest()
+    xhttp.onreadystatechange = function(){
+        if(xhttp.readyState == 4 && xhttp.status == 200){
+            document.getElementById("cidades").innerHTML = "<pre>" + xhttpresponseText+"</pre>"
+        }
+    }
+    xhttp.open("GET", "ajax1_cidades.txt", true)
+    xhttp.send()
+   }
+}
